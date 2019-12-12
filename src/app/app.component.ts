110fab282ba62a03ea796e0f6fdc2b6e169f6241
salemm-idr <ejuarez@idr-tech.com>
2019-12-12 15:24:08 -0600

    :fire::rocket: first commit ninja course :skull:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //decorator object with options
+  selector: "app-root", //selector for the html browser
+  templateUrl: "./app.component.html", //selector for the template view
+  styleUrls: ["./app.component.css"] //selector for the style template use override for the bootstrap classes
 })
 export class AppComponent {
-  title = 'ninja-course';
+  //logic for component
+  title = "ninja-course";
 }
